84bf5c8d631f8799d30bdec3e8e7a0d071efa9dc
rola-gh <rolla.ghanima@gmail.com>
2021-05-28 12:14:29 +0300

    file structure

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <Nav/>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import Nav from "./layout/nav";
+
+export default {
+  name: 'Home',
+  components: {Nav},
+  props: {
+    msg: String
+  },
+  component:{
+    Nav
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/layout/nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-light ">
+            <div class="container-fluid">
+                <a class="navbar-brand px-5" href="#">Blog</a>
+                <button type="button" class="btn btn-primary ">Create</button>
+            </div>
+        </nav>
+    </div>
+</template>
+<script>
+	export default {
+		name:"Nav",
+	}
+</script>
